081dc0c241fb2ebd24c1dcd1cbb67f5a0d4bd2fc
build <build@infra-1.local.wdha>
2016-02-23 20:55:22 +0100

    Add event support.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var Promise      = require('bluebird'),
     parseLinks   = require('parse-links'),
     url          = require('url'),
     objectAssign = require('object-assign'),
-    arrayFlatten = require('array-flatten');
+    arrayFlatten = require('array-flatten'),
+    util         = require('util'),
+    EventEmitter = require('events').EventEmitter;
 
 var getTags = function(pageNo, repoId, github) {
   return github.repos.getTagsAsync(
@@ -24,39 +26,68 @@ var getCommit = function(tag, repoId, github) {
   );
 };
 
-githubTags = function(repoId, github) {
+var GithubTags  = function() {
+  this.self     = this;
+  this.tagsAll        = 0;
+  this.tagsDone       = 0;
+  this.tagCommitsDone = 0;
+};
+
+GithubTags.prototype.fetch = function(repoId, github) {
+
   Promise.promisifyAll(github.repos);
+
+  var self = this.self;
 
   // Current github API (v3) doesn't support sorting tags (e.g. by their creation date).
   return getTags(1, repoId, github)
   .then(function(firstTags) {
+
+    self.tagsAll   += firstTags.length;
+    self.emit('page', 1);
+
     var lastPageUrl = parseLinks(firstTags.meta.link).last;
     var lastPageNo  = url.parse(lastPageUrl, true).query.page;
 
-    var pageNos = [];
-    for(var pageNo = 2; pageNo <= lastPageNo; pageNo++) {
+    var pageNos     = [];
+    for(var pageNo  = 2; pageNo <= lastPageNo; pageNo++) {
       pageNos.push(pageNo);
     }
-
-   // tagPages.push(firstTags); // add own tags (from this 1st lookup)
 
     return Promise
     .map(pageNos, function(pageNo) { // each page no
       return getTags(pageNo, repoId, github)
+      .then(function(tags) {
+        self.tagsAll  += tags.length;
+        self.emit('page', pageNo);
+        return tags;
+      })
       .map(function(tag) {
-        return getTagCommit(tag, repoId, github);
+        self.tagsDone += 1;
+        self.emit('tag', tag);
+        return getTagCommit(tag, repoId, github)
+        .then(function(tagCommit) {
+          self.tagCommitsDone += 1;
+          self.emit('tag-commit', tagCommit);
+        });
       });
     })
 
-    .then(function(tagsCommits) {
-
-      // Also add the first tags
+    .then(function(tagCommits) {
+      // Also add the tags from the 1st page
       return Promise
       .map(firstTags, function(tag) {
-        return getTagCommit(tag, repoId, github);
+        self.tagsDone += 1;
+        self.emit('tag', tag);
+        return getTagCommit(tag, repoId, github)
+        .then(function(tagCommit) {
+          self.tagCommitsDone += 1;
+          self.emit('tag-commit', tagCommit);
+          return tagCommit;
+        });
       })
-      .then(function(firstTagsCommits) {
-        return tagsCommits.concat(firstTagsCommits);
+      .then(function(firstTagCommits) {
+        return tagCommits.concat(firstTagCommits);
       });
 
     });
@@ -67,4 +98,6 @@ githubTags = function(repoId, github) {
   });
 };
 
-module.exports = githubTags;
+util.inherits(GithubTags, EventEmitter);
+
+module.exports = GithubTags;
